18d7394881e5a49a5f2994a70d0db4319df038d6
kwonheejeong <gmlwjd9405@naver.com>
2018-12-06 22:37:14 +0900

    First commit

---
+++ src/main/java/com/hee/QnaBoard2Application.java
@@ -0,0 +1,12 @@
+package com.hee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QnaBoard2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QnaBoard2Application.class, args);
+    }
+}
