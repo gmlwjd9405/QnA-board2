800417f8dd2a2fafbdfb1d7db7cef69ed1a71878
kwonheejeong <gmlwjd9405@naver.com>
2018-12-07 17:10:21 +0900

    Add test code about saving requestDto and Update Controller

---
+++ src/test/java/com/hee/service/PostServiceTest.java
@@ -0,0 +1,48 @@
+package com.hee.service;
+
+import com.hee.domain.posts.Posts;
+import com.hee.domain.posts.PostsRepository;
+import com.hee.domain.posts.PostsSaveRequestDto;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostServiceTest {
+
+    @Autowired
+    private PostsService postsService;
+
+    @Autowired
+    private PostsRepository postsRepository;
+
+    @After
+    public void cleanup () {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void saveDtoDataInPostsTableTest() {
+        //given
+        PostsSaveRequestDto dto = PostsSaveRequestDto.builder()
+                .author("jojoldu@gmail.com")
+                .content("테스트")
+                .title("테스트 타이틀")
+                .build();
+
+        //when
+        postsService.save(dto);
+
+        //then
+        Posts posts = postsRepository.findAll().get(0);
+        assertThat(posts.getAuthor()).isEqualTo(dto.getAuthor());
+        assertThat(posts.getContent()).isEqualTo(dto.getContent());
+        assertThat(posts.getTitle()).isEqualTo(dto.getTitle());
+    }
+}
