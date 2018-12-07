8c8814fcde123e67d00ab4e9bc4ace1359b9f86d
kwonheejeong <gmlwjd9405@naver.com>
2018-12-07 17:56:53 +0900

    mAdd main response dto and Update posts service and main controller

--- src/main/java/com/hee/service/PostsService.java
+++ src/main/java/com/hee/service/PostsService.java
@@ -1,10 +1,14 @@
 package com.hee.service;
 
-import com.hee.domain.posts.PostsRepository;
-import com.hee.domain.posts.PostsSaveRequestDto;
+import com.hee.dto.PostsMainResponseDto;
+import com.hee.repository.PostsRepository;
+import com.hee.dto.PostsSaveRequestDto;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @Service
@@ -14,5 +18,12 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto dto){
         return postsRepository.save(dto.toEntity()).getId();
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsMainResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc()
+                .map(PostsMainResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
