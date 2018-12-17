1256628c59dacc8a07573bd531c89acdf4e3ac32
kwonheejeong <gmlwjd9405@naver.com>
2018-12-17 15:38:05 +0900

    Update setting for non-disruptive deployment

--- execute-deploy.sh
+++ execute-deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 echo "> deploy.sh를 백그라운드로 실행, 로그나 기타 내용을 남기지 않음"
-/home/ubuntu/app/travis/deploy.sh > /dev/null 2> /dev/null < /dev/null &
+# /home/ubuntu/app/travis/deploy.sh > /dev/null 2> /dev/null < /dev/null &
+/home/ubuntu/app/nonstop/deploy.sh > /dev/null 2> /dev/null < /dev/null &
